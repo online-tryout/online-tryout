4acdd33bff0e9982b451aac06f65e83db4baa19b
ana117 <63540951+ana117@users.noreply.github.com>
2024-06-04 10:34:57 +0700

    feat: create landing page

--- components/home/home.tsx
+++ components/home/home.tsx
@@ -4,8 +4,6 @@ import { useTheme } from "next-themes";
 import { FC, useEffect } from "react";
 import { Button } from "../ui/button";
 import { useRouter } from "next/navigation";
-import WebIcon from "./components/web-icon";
-import AppleIcon from "./components/apple-icon";
 
 const Home: FC = () => {
   const { setTheme } = useTheme();
@@ -16,49 +14,28 @@ const Home: FC = () => {
   }, [setTheme]);
 
   return (
-    <div className="grid max-h-[calc(100vh-64px)] w-full grid-cols-2">
-      <div className="h-[calc(100vh-64px)] w-full border-r">
-        <video
-          src="/video/people.mp4"
-          className="h-full w-full object-cover"
-          autoPlay
-          loop
-          muted
-          style={{
-            transition: "all 0.8s ease-in-out 0s",
-          }}
-        />
-      </div>
-      <div className="relative">
-        <div className="absolute top-1/2 flex w-full w-full -translate-y-1/2 flex-col items-end justify-end gap-2 p-8 text-end">
-          <div className="text-5xl font-bold">Create Your 3D Asset Now</div>
-          <div className="text-lg font-medium">
-            Developed with a novel 3D rendering model,{" "}
-            <span className="underline">Gaussian Splatting</span>
+    <div className="flex w-full h-[calc(100vh-66px)]">
+      <div className="h-full w-full border-r">
+        <div className="flex flex-col items-center justify-center h-full gap-4 p-8 text-center">
+          <div className="text-5xl font-bold">Welcome to Online Tryout</div>
+          <div className="text-lg font-medium text-center">
+            Prepare for Success, Excel in Every Test!
           </div>
-          <div className="w-full">
-            <div className="mb-1">Try it on:</div>
-            <div className="flex w-full justify-end gap-2 font-semibold">
-              <Button
-                className="flex gap-2 px-4"
-                variant={"outline"}
-                size={"lg"}
-              >
-                <AppleIcon />
-                IOS
-              </Button>
-              <Button
-                variant={"outline"}
-                className="flex gap-2 px-4"
-                size={"lg"}
-                onClick={() => routes.push("/dashboard")}
-              >
-                <WebIcon />
-                Web
-              </Button>
-            </div>
+          <div className="flex gap-4">
+            <Button onClick={() => routes.push("/auth")}>
+              Get Started
+            </Button> 
           </div>
         </div>
+      </div>
+      
+      <div className="flex flex-col items-center justify-center w-full h-full p-8 gap-16 text-center">
+        <p>
+          Join thousands of high school students across Indonesia in our comprehensive online tryouts. Elevate your academic performance, master key subjects, and achieve your goals with our expertly crafted practice exams.
+        </p>
+        <p className="font-semibold">
+          Start your journey to academic excellence today!
+        </p>
       </div>
     </div>
   );
