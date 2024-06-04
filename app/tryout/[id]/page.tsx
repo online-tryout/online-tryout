35e4388f108df7452cd622e49b88a05aa1546333
ana117 <63540951+ana117@users.noreply.github.com>
2024-06-04 12:34:54 +0700

    fix: to list

--- app/tryout/[id]/page.tsx
+++ app/tryout/[id]/page.tsx
@@ -1,11 +1,15 @@
 import TryoutList from "@/components/tryout/to-list";
-import { getListTryout } from "../action";
+import { getListTryout, getMyTransactions } from "../action";
+import { cookies } from "next/headers";
 
 export default async function TryoutDetailPage({
   params,
 }: {
   params: { id: string };
 }) {
-  const { tryouts } = await getListTryout();
-  return <TryoutList tryouts={tryouts} />;
+  const tryouts = await getListTryout();
+
+  const userId = cookies().get("userId")?.value ?? "";
+  const transactions = await getMyTransactions(userId);
+  return <TryoutList tryouts={tryouts} transactions={transactions} />;
 }
