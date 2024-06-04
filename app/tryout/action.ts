329e1302ca36a0514260bedcbab1f08096529b72
ana117 <63540951+ana117@users.noreply.github.com>
2024-06-04 11:45:57 +0700

    feat: add get my transactions

--- app/tryout/action.ts
+++ app/tryout/action.ts
@@ -16,9 +16,28 @@ export async function getListTryout(): Promise<{
         Authorization: `Bearer ${cookies().get("accessToken")?.value}`,
       },
     });
+    return data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
 
-    console.log(data);
-
+export async function getMyTransactions(userId: string): Promise<{
+  transactions: any[];
+  message: string;
+}> {
+  "use server";
+  try {
+    console.log(`/api/payment/transactions/user/${userId}`)
+    const { data } = await serverAxios.get<{
+      transactions: any[];
+      message: string;
+    }>(`/api/payment/transactions/user/${userId}`, {
+      headers: {
+        Authorization: `Bearer ${cookies().get("accessToken")?.value}`,
+      },
+    });
     return data;
   } catch (error) {
     console.log(error);
